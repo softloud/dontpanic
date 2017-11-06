20b9fb1bd7a49a366dff6fe7d268c9f106efea3a
Charles T. Gray <C.Gray@latrobe.edu.au>
2017-11-06 11:50:40 +1100

    :cat: title_date function for .Rpres :notes:

---
+++ R/dontpanic-package.R
@@ -0,0 +1,15 @@
+#' @importFrom magrittr %>%
+#'
+
+#' @export
+magrittr::`%>%`
+
+#' Coding equivalent of a towel.
+#'
+#' My helper functions are parked here. Use at own risk.
+#'
+#' @docType package
+#' @name cricinfo
+#' @keywords package
+NULL
+#"_PACKAGE"
