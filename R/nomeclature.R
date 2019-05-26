09226288ac464aa2b69420581efe20d8d4df94a1
cantabile <charlestigray@gmail.com>
2019-05-26 13:25:31 +1000

    #10 updates unicode symbols

--- R/nomeclature.R
+++
@@ -1,9 +0,0 @@
-#' lifeswork nomenclature
-#' @name nomenclature
-NULL
-
-#' musical notation shortcuts for output in .Rmd; use as in-line R.
-#' @describeIn nomeclature
-#' @export
-quaver <- function(){knitr::asis_output("&#9834;")}
-
