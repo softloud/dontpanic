39e2bc8927edf3b63ab7818b00552875a631e78b
Charles T. Gray <charlestigray@gmail.com>
2019-12-21 07:26:41 +1100

    title_date, also close #7

---
+++ R/title_date.R
@@ -0,0 +1,22 @@
+#' Format Date for .RPres
+#'
+#' Return a nicely formatted title date for RMarkdown presentations.
+#'
+#' @param this_date Optional argument to set date with a `date` object.
+#' Otherwise defaults to current date.
+#'
+#' @export
+#'
+#' @examples
+#' # title_date()
+#' # title_date("2017-11-04")
+
+title_date <- function(this_date = NULL) {
+  if (is.null(this_date)) this_date <- lubridate::today()
+  paste(
+    this_date %>% lubridate::wday(label = T, abbr = F),
+    this_date %>% lubridate::day(),
+    this_date %>% lubridate::month(label = T, abbr = F),
+    this_date %>% lubridate::year()
+  )
+}
