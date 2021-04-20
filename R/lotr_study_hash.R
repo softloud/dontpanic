6b42270da363818e902787a0534299a9155825f6
charles <charlestigray@gmail.com>
2021-04-20 22:45:47 +0100

    update

--- R/lotr_study_hash.R
+++ R/lotr_study_hash.R
@@ -7,32 +7,89 @@
 #' need to filter duplicates if want more.
 #'
 #' @param dat A data frame with `study_col` column.
-#' @param study_col Column that uniquely identifies the study.
+#' @param study_col Column indicating which study the observations recorded in
+#' that row came from. This will replace them with star wars characters.
+#' @param trt_col Column indicating which treatments. Replaced with
+#' lotr characters.
 #' @param sep Separator between name and year, defaults to "_".
+#' @param max_obs Gotta put a number into the year sampler.
 #'
 #' @export
 
 
-lotr_study_hash <- function(dat, study_col, sep = "_") {
-  # unique studies
-  studies <-
+lotr_study_hash <-
+  function(dat,
+           study_col,
+           trt_col,
+           sep = "_",
+           max_obs = 500) {
+    # unique studies
+    studies <-
+      dat %>%
+      dplyr::pull({
+        {
+          study_col
+        }
+      }) %>%
+      unique()
+
+    # unique treatments
+    treatments <-
+      dat %>%
+      dplyr::pull({
+        {
+          trt_col
+        }
+      }) %>%
+      unique()
+
+    # create key table from lotr characters for treatments
+    key_trt <-
+      tibble::tibble(trt = treatments,
+                     lotr = lotr_study %>%
+                       head(length(treatments)) %>%
+                       pull(author)) %>%
+      dplyr::mutate(lotr = dplyr::if_else(trt == "placebo", trt, lotr)) %>%
+      dplyr::rename({
+        {
+          trt_col
+        }
+      } := trt)
+
+    # and hashes for studies
+    key_studies <-
+      tibble::tibble(
+        study = studies,
+        sw = dplyr::starwars %>%
+          head(length(studies)) %>%
+          pull(name) %>%
+          str_extract("[\\w|-]+") %>%
+          sample(size = length(studies), replace = TRUE),
+        year = sample(
+          x = seq(1800, 1800 + max_obs),
+          size = length(studies),
+          replace = FALSE
+        )
+      ) %>%
+      unite(col = "sw", sw, year, sep = sep) %>%
+      rename({{study_col}} := study)
+
+    # join fake id to existing dataset
     dat %>%
-    dplyr::pull({{study_col}}) %>%
-    unique()
+      dplyr::left_join(key_trt) %>%
+      dplyr::select(-{
+        {
+          trt_col
+        }
+      }) %>%
+      dplyr::rename({
+        {
+          trt_col
+        }
+      } := lotr) %>%
+      dplyr::left_join(key_studies) %>%
+      dplyr::select(-{{study_col}}) %>%
+      dplyr::rename({{study_col}} := sw) %>%
+      dplyr::select({{study_col}}, {{trt_col}}, dplyr::everything())
 
-  #create key table from lotr characters
-  key <-
-    lotr_study %>%
-    head(length(studies)) %>%
-    tidyr::unite("fake_id", author, year, sep = sep) %>%
-    dplyr::mutate({{study_col}} := studies) %>%
-    dplyr::select({{study_col}}, fake_id)
-
-  # join fake id to existing dataset
-  dat %>%
-    dplyr::left_join(key) %>%
-    dplyr::select(-{{study_col}}) %>%
-    dplyr::rename(
-      {{study_col}} := fake_id
-    )
-}
+  }
